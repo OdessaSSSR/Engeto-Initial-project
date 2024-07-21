474780a734fce040104b9d267666c1c4e1081b4b
Vadym Drahý <167234646+OdessaSSSR@users.noreply.github.com>
2024-07-21 18:03:04 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+const payment = document.querySelector(".platba");
+const sale = document.querySelector(".sleva");
+const robe = document.querySelector(".saty");
+const advert = document.querySelector(".reklam");
+const description = document.querySelector(".description");
+const reset = document.querySelector(".navrat");
+
+const body = document.querySelector("body");
+const cage = document.querySelector(".background");
+const picture = document.querySelector("img");
+
+const item = document.querySelector(".kus");
+
+const home = document.querySelector(".return");
+const mode = document.querySelector(".btn-toggle");
+
+var audio = new Audio();
+audio.preload = "auto";
+audio.src = "/Císařovy nové šaty/images+/Beethoven-simfoniya-5.mp3";
+
+payment.addEventListener("click", () => {
+  if (robe.style.opacity === "1") {
+    robe.style.opacity = "0";
+  } else {
+    robe.style.opacity = "1";
+    audio.play();
+  }
+
+  payment.style.display = "none";
+  sale.style.display = "none";
+  advert.style.display = "none";
+  reset.style.display = "block";
+  description.innerHTML = "Státní pokladna je prázdná,<br>Veličenstvo...";
+  description.style.backgroundColor = "yellow";
+});
+
+mode.addEventListener("click", () => {
+  document.body.classList.toggle("light-theme");
+
+  // picture.classList.remove(".background");
+  // picture.classList.add(".change");
+
+  picture.classList.toggle(".change");
+  console.log(picture.className);
+});
